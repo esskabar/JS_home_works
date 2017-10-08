71a04d013b5102508c12a388c07458df45090c2f
maxim <max.gavrushenko.tirare@gmail.com>
2017-10-08 20:04:11 +0300

    NPM install add arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 console.log(1111);
+
+    var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+    var array2 = [-1,-8,-2];
+    var array3 = [1,7,3];
+    var array4 = [1,undefined,3,5,-3];
+    var array5 = [1,NaN,3,5,-3];

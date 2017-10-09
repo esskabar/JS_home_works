23a49bba793c1a684547fb0b6f034fa02166f238
maxim <max.gavrushenko.tirare@gmail.com>
2017-10-09 22:38:11 +0300

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-console.log(1111);
-
-    var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-    var array2 = [-1,-8,-2];
-    var array3 = [1,7,3];
-    var array4 = [1,undefined,3,5,-3];
-    var array5 = [1,NaN,3,5,-3];
-
-
-
 
 function getMaxOfArray(numArray) {
     return Math.max.apply(null, numArray);
@@ -18,9 +8,39 @@ function getMinOfArray(numArray) {
 }
 
 
-var index, len;
-var sumarray = [array1,array2,array3,array4,array5];
-for (index = 0, len = sumarray.length; index < len; ++index) {
-    console.log(sumarray[index]);
+    var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+        array2 = [-1,-8,-2];
+        array3 = [1,7,3];
+        array4 = [1,undefined,3,5,-3];
+        array5 = [1,NaN,3,5,-3];
 
-}
+ 
+
+    function arrayCheck(count){
+        var max = Number.NEGATIVE_INFINITY ;
+        var min= Number.POSITIVE_INFINITY ;
+        var sum = 0;
+
+        for(var i = 0; i < count.length; i++){
+
+            if( typeof (count[i]) !== 'undefined' && !isNaN(count[i])){
+                if(max < count[i]) max = count[i];
+                if(min > count[i]) min = count[i];
+                sum = sum + count[i];
+            }
+        }
+
+        return {
+            'max': max,
+            'min': min,
+            'sum': sum
+        }
+
+    }
+
+    console.log(arrayCheck(array1));
+    console.log(arrayCheck(array2));
+    console.log(arrayCheck(array3));
+    console.log(arrayCheck(array4));
+    console.log(arrayCheck(array5));
+

7e5d6cbb673f9e4709a8900711b70eff40ec7867
maxim <max.gavrushenko.tirare@gmail.com>
2017-10-18 14:37:12 +0300

    clear code

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,27 @@
-
-    var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-        array2 = [-1,-8,-2];
-        array3 = [1,7,3];
-        array4 = [1,undefined,3,5,-3];
-        array5 = [1,NaN,3,5,-3];
-        array6 =  ['erefs',5342,-653,'2'];
-
+var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,'dssadhkbvasj'];
+array2 = [-1,-8,-2,null, true,false];
+array3 = [1,7,3];
+array4 = [1,undefined,3,5,-3];
+array5 = [1,NaN,3,5,-3];
+array6 =  ['erefs',5342,-653,'2',true,null, undefined];
 
 
-    function arrayCheck(count){
-        var max = Number.NEGATIVE_INFINITY ;
-        var min= Number.POSITIVE_INFINITY ;
-        var sum = 0;
 
-        for(var i = 0; i < count.length; i++){
+function arrayCheck(count){
+    var max = null ;
+    var min= null ;
+    var sum = 0;
 
-            if( typeof (count[i]) !== 'undefined' && !isNaN(count[i])){
+    for(var i = 0; i < count.length; i++){
 
-                if(max < count[i]) max = count[i];
-                if(min > count[i]) min = count[i];
-                sum = sum + parseInt(count[i]);
-            }
+        if( typeof (count[i]) === 'number' && !isNaN(count[i])){
 
+            if(max < count[i]) max = count[i];
+            if(min > count[i]) min = count[i];
+            sum = sum + (count[i]);
         }
+
+    }
 
         return {
             'max': max,

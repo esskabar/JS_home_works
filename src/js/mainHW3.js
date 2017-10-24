d8f289c58973d09673205ad0b14d6a710247ecdd
maxim <max.gavrushenko.tirare@gmail.com>
2017-10-24 17:47:30 +0300

    myMap method

--- src/js/mainHW3.js
+++ src/js/mainHW3.js
@@ -21,16 +21,65 @@ Array.prototype.myForEach = function (callback , arr) {
 
 
 array.myForEach(function (num, index) {
-    console.log( num, index);
+   // console.log( num, index);
 });
 
 
 
 
 
-function myMap (){
 
-}
+Array.prototype.myMap = function(callback, thisArg) {
+
+    var T, A, k;
+
+    if (this == null) {
+        throw new TypeError(' this is null or not defined');
+    }
+
+    var O = Object(this);
+
+    var len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    if (arguments.length > 1) {
+        T = thisArg;
+       // console.log(thisArg);
+    }
+
+    A = new Array(len);
+
+
+    k = 0;
+
+    while (k < len) {
+
+        var kValue, mappedValue;
+
+        if (k in O) {
+
+            kValue = O[k];
+
+            mappedValue = callback.call(T, kValue, k, O);
+
+            A[k] = mappedValue;
+        }
+
+        k++;
+    }
+
+    return A;
+};
+
+
+var roots = array.myMap(Math.sqrt);
+
+console.log(roots);
+
+
 
 function mySort (){
 
